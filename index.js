3904884d8e83f768024dce6bffafa804137e2855
Bill <aldridgewilliam01@gmail.com>
2023-10-03 11:03:02 -0600

    Updated HTML and JS

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-var todo = []
+var reminder = [];
+var todo = document.getElementByID("tasker")
+var listBox = document.getElementByID("cabinet")
+todo.listBox
+
 const addto = () => {
-  let inputtext = tasker.value;
-  let btn = (document.querySelector(".addbutton"));
-  btn.addEventListener("click", () => let newItem = document.createElement('li'))
-  newItem.innerHTML = "New item"
-  todo.appendChild(newItem);
+      if(todo.value === '') {
+      reminder.push(todo.value)
+
+      }
+      else{
+
+        alert("Did you forget to type something?")
+      }
+
 }
+
+
+
+function stuff () {
+      for (let i = 0; i < reminder.length; i++){
+
+        }
+
+}
+console.log(reminder);
+//2.3
+/*
+let newLi = document.createElement('li');
+        newLi.innerHTML = todo.value;
+        newLi.appendChild(li)
+*/
+
+
+
+/*
 console.log(inputtext)
 console.log(todo)
+
+let inputtext = tasker.value;
+let btn = (document.querySelector(".addbutton"));
+btn.addEventListener("click", () => let newItem = document.createElement('li'))
+newItem.innerHTML = "New item"
+todo.appendChild(newItem);
+
+*/
