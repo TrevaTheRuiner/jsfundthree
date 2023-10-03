48ba759e1ed4d1e68095b507a016495f6a0ba99a
Bill <aldridgewilliam01@gmail.com>
2023-10-03 15:22:32 -0600

    Group 2 submitsion

--- index.js
+++ index.js
@@ -16,43 +16,39 @@ const addto = () => {
 const stuff = () => {
       for (let i = 0; i < reminder.length; i++) {
         let shelflist = document.querySelector('#shelf1')
-        let newItem = document.createElement('input')
+        let newItem = document.createElement('li')
         newItem.innerHTML = reminder[i]
         shelflist.appendChild(newItem)
         }
 }
 
-const remove = () => {
-
-  if {
-
-  } else {
-
-  }
-
+function remove() {
+  var doc = document.getElementById("shelf1");
+doc.removeChild(doc.childNodes[0]);
 }
 
-console.log(reminder);
+todo.addEventListener('click', function(e) {
+  {
+    if (e.target.tagName === 'li'); {
+      e.target.classList.toggle("checked");
+    elseif (e.target.tagName === "newItem"); {
+      e.target.parentElement.remove();
+    }
+  }
+}
+}
+,console.log(reminder)
 
 
 
-//2.3
-/*
-let newLi = document.createElement('li');
-        newLi.innerHTML = todo.value;
-        newLi.appendChild(li)
-*/
 
 
+// console.log(inputtext)
+// console.log(todo)
 
-/*
-console.log(inputtext)
-console.log(todo)
+// let inputtext = tasker.value;
+// let btn = (document.querySelector(".addbutton"));
+// btn.addEventListener("click", () => let newItem = document.createElement('li'))
+// newItem.innerHTML = "New item"
+// todo.appendChild(newItem);
 
-let inputtext = tasker.value;
-let btn = (document.querySelector(".addbutton"));
-btn.addEventListener("click", () => let newItem = document.createElement('li'))
-newItem.innerHTML = "New item"
-todo.appendChild(newItem);
-
-*/
